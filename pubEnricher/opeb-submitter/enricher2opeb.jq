e346e1b210e8233f7fc3f9d1d7826c85d668bfa1
José María Fernández <jose.m.fernandez@bsc.es>
2018-05-16 13:00:06 +0200

    Added the results submitter to translate and upload results to OpenEBench

--- pubEnricher/opeb-submitter/enricher2opeb.jq
+++ pubEnricher/opeb-submitter/enricher2opeb.jq
@@ -6,29 +6,27 @@ map(
 	|
 	{
 		"@id": $id | split("/") | ( .[4] |= "metrics" ) | join("/"),
-		"project": {
-			"publications": (
-				$entry_pubs | map(
-					{
-						"entries": .["found_pubs"] | map (
-							{
-								"title": .title?,
-								"year": .year?,
-								"ref_count": (.reference_count//0),
-								"cit_count": (.citation_count//0),
-								"refs": (.reference_stats//[]),
-								"citations": (.citation_stats//[])
-							}
-						)
-					}
-					+
-					if has("doi") then {"doi": .doi } else {} end
-					+
-					if has("pmid") then {"pmid": .pmid } else {} end
-					+
-					if has("pmcid") then {"pmcid": .pmcid } else {} end
-				)
+		"project.publications": (
+			$entry_pubs | map(
+				{
+					"entries": .["found_pubs"] | map (
+						{
+							"title": .title?,
+							"year": .year?,
+							"ref_count": (.reference_count//0),
+							"cit_count": (.citation_count//0),
+							"refs": (.reference_stats//[]),
+							"citations": (.citation_stats//[])
+						}
+						+
+						if has("doi") then {"doi": .doi } else {} end
+						+
+						if has("pmid") then {"pmid": .pmid } else {} end
+						+
+						if has("pmcid") then {"pmcid": .pmcid } else {} end
+					)
+				}
 			)
-		}
+		)
 	}
 )
--- pubEnricher/opeb-submitter/result_submitter.bash
+++ pubEnricher/opeb-submitter/result_submitter.bash
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-OPEB_METRICS_BASE=https://openebench.bsc.es/monitor/metrics
-OPEB_METRICS_USER=pubenricher
-OPEB_METRICS_PASS=ObKavVar7OvImwufineds
+OPEB_METRICS_BASE=https://openebench.bsc.es/monitor/metrics/
 
-scriptdir="$(basedir "$0")"
+scriptdir="$(dirname "$0")"
 
 case "$scriptdir" in
 	/*)
@@ -15,28 +13,18 @@ case "$scriptdir" in
 		;;
 esac
 
-#JQTRANS=<<EOF
-#{
-#	"@id": .["@id"],
-#	"project": {
-#		"publications": ( .entry_pubs | { "doi": .doi } )
-#	}
-#}
-#EOF
-#
-#jq "$JQTRANS" entry_12873.json
-
-
-if [ $# > 0 ]; then
-#	for A in "$@" ; do
-		# Get the id
-#		tool_id="$(jq -r '.["@id"]' "$A" | cut -d '/' -f 6- )"
-#		
-#		jq "$JQTRANS" "$A"
-		
-#		echo "Storing tool ${tool_id}"
+if [ $# > 1 ]; then
+	config="$1"
+	shift
+	# Loading the credentials
+	source "$config"
+	if [ -n "$OPEB_METRICS_USER" -a -n "$OPEB_METRICS_PASS" ]; then
 		jq --slurp -f "$scriptdir"/enricher2opeb.jq "$@" | \
 		curl -v -X PATCH -u "${OPEB_METRICS_USER}":"${OPEB_METRICS_PASS}" -H 'Content-Type: application/json' \
 		"${OPEB_METRICS_BASE}" -d "@-"
-#	done
+	else
+		echo "ERROR: The config file must declare the OPEB_METRICS_USER and OPEB_METRICS_PASS variables, which are the credentials to OpenEBench" 1>&2
+	fi
+else
+	echo "Usage: $0 {config_file} {json_result}+" 1>&2
 fi
