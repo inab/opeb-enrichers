47e04c5098080a6b319bda8abc3507beb2ac2804
José María Fernández <jose.m.fernandez@bsc.es>
2020-03-30 20:05:39 +0200

    Improved data submitter

--- pubEnricher/opeb-submitter/result_submitter.bash
+++ pubEnricher/opeb-submitter/result_submitter.bash
@@ -8,6 +8,8 @@ OPEB_METHOD=POST
 
 OPEB_METRICS_BASE=https://${OPEB_HOST}/monitor/metrics/
 
+set -e
+
 scriptdir="$(dirname "$0")"
 
 case "$scriptdir" in
@@ -19,18 +21,68 @@ case "$scriptdir" in
 		;;
 esac
 
+eecho () {
+	echo "$@" 1>&2
+}
+
+doProcessing () {
+	# Classifying the input
+	local -a files
+	local -a dirs
+	local -a discarded
+	for file in "$@" ; do
+		if  [ -r "$file" ] ; then
+			if [ -f "$file" ] ; then
+				files+=( "$file" )
+			elif [ -d "$file" ] ; then
+				dirs+=( "$file" )
+			else
+				discarded+=( "$file" )
+			fi
+		else
+			discarded+=( "$file" )
+		fi
+	done
+	
+	if [ ${#discarded[@]} -gt 0 ] ; then
+		eecho "DISCARDED / UNREADABLE: ${#discarded[@]}"
+		eecho $'\t'"${discarded[@]}"
+		eecho
+	fi
+	
+	if [ ${#files[@]} -gt 0 ] ; then
+		eecho "NUM FILES TO PROCESS: ${#files[@]}"
+		eecho
+		jq --slurp --arg host "$OPEB_HOST" -f "$scriptdir"/enricher2opeb.jq "${files[@]}" | \
+		curl -v -X "${OPEB_METHOD}" -u "${OPEB_METRICS_USER}":"${OPEB_METRICS_PASS}" -H 'Content-Type: application/json' \
+		"${OPEB_METRICS_BASE}" -d "@-"
+	fi
+	
+	# Last, the directories
+	if [ ${#dirs[@]} -gt 0 ] ; then
+		eecho "DIRS TO PROCESS: ${#dirs[@]}"
+		eecho
+		for dir in "${dirs[@]}" ; do
+			eecho "PROCESSING DIR $dir"
+			eecho
+			
+			doProcessing "$dir"/*
+		done
+	fi
+}
+
 if [ $# -gt 1 ]; then
 	config="$1"
 	shift
+	
 	# Loading the credentials
 	source "$config"
+	
 	if [ -n "$OPEB_METRICS_USER" -a -n "$OPEB_METRICS_PASS" ]; then
-		jq --slurp --arg host "$OPEB_HOST" -f "$scriptdir"/enricher2opeb.jq "$@" | \
-		curl -v -X "${OPEB_METHOD}" -u "${OPEB_METRICS_USER}":"${OPEB_METRICS_PASS}" -H 'Content-Type: application/json' \
-		"${OPEB_METRICS_BASE}" -d "@-"
+		doProcessing "$@"
 	else
-		echo "ERROR: The config file must declare the OPEB_METRICS_USER and OPEB_METRICS_PASS variables, which are the credentials to OpenEBench" 1>&2
+		eecho "ERROR: The config file must declare the OPEB_METRICS_USER and OPEB_METRICS_PASS variables, which are the credentials to OpenEBench"
 	fi
 else
-	echo "Usage: $0 {config_file} {json_result}+" 1>&2
+	eecho "Usage: $0 {config_file} {json_result or dir}+"
 fi
