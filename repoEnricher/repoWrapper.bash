d62bbcb30c0f85dbdd8d66e0eb19a153b7d52c0f
José Mª Fernández <jose.m.fernandez@bsc.es>
2020-09-02 13:01:33 +0200

    Tools list download and compression have been integrated into the wrapper

--- repoEnricher/repoWrapper.bash
+++ repoEnricher/repoWrapper.bash
@@ -17,16 +17,26 @@ esac
 if [ $# -eq 2 ] ; then
 	workDir="$1"
 	toolsFile="$2"
+	toolsFileXZ="${toolsFile}.xz"
 
-	# Only when the file exists
-	if [ -f "$toolsFile" ] ; then
-		# But not the working directory
-		if [ ! -d "$workdDir" ] ; then
-			mkdir -p "$workDir"
-			perl "${SCRIPTDIR}"/repoEnricher.pl -C "${SCRIPTDIR}"/cron-config.ini -D "$workDir" --use-opeb "$toolsFile"
-			echo "${SHELL}" "${SCRIPTDIR}"/opeb-submitter/repo_result_submitter.bash "${SCRIPTDIR}"/opeb-submitter/cron-submitter.ini "$workDir"
+	if [ ! -f "$toolsFileXZ" ] ; then
+		if [ ! -f "$toolsFile" ] ; then
+			mkdir -p "$(dirname "$toolsFile")"
+			wget -nv -O "$toolsFile" https://openebench.bsc.es/monitor/rest/search
 		fi
+		xz -9 -T 0 "${toolsFile}"
 	fi
+
+	# Only erase when the manifest file does not exist
+	if [ ! -f "${workDir}/manifest.json" ] ; then
+		rm -rf "${workDir}"
+	fi
+	# But not the working directory
+	if [ ! -d "$workDir" ] ; then
+		mkdir -p "$workDir"
+		perl "${SCRIPTDIR}"/repoEnricher.pl -C "${SCRIPTDIR}"/cron-config.ini -D "$workDir" --use-opeb "$toolsFileXZ"
+	fi
+	echo "${SHELL}" "${SCRIPTDIR}"/opeb-submitter/repo_result_submitter.bash "${SCRIPTDIR}"/opeb-submitter/cron-submitter.ini "$workDir"
 else
 	echo "ERROR: This script needs two parameters: a workdir and the input tools file" 1>&2
 fi
