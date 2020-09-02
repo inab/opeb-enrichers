288b84cb5f78ca1d758a795c0eecddb7afc75d53
José María Fernández <jose.m.fernandez@bsc.es>
2020-09-03 00:38:27 +0200

    Added cron wrapper for periodic OpenEBench updates

---
+++ pubEnricher/pubWrapper.bash
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+exec > /tmp/prueba-pub.txt 2>&1
+
+set -e
+SCRIPTDIR="$(dirname "$0")"
+
+case "${SCRIPTDIR}" in
+	/*)
+		true
+		;;
+	*)
+		SCRIPTDIR="${PWD}/${SCRIPTDIR}"
+		;;
+esac
+
+if [ $# -eq 2 ] ; then
+	workDir="$1"
+	toolsFile="$2"
+	toolsFileXZ="${toolsFile}.xz"
+
+	if [ ! -f "$toolsFileXZ" ] ; then
+		if [ ! -f "$toolsFile" ] ; then
+			mkdir -p "$(dirname "$toolsFile")"
+			wget -nv -O "$toolsFile" https://openebench.bsc.es/monitor/rest/search
+		fi
+		xz -9 -T 0 "${toolsFile}"
+	fi
+	
+	# Only erase when the manifest file does not exist
+	if [ ! -f "${workDir}/manifest.json" ] ; then
+		rm -rf "${workDir}"
+	fi
+	# But not the working directory
+	if [ ! -d "$workDir" ] ; then
+		mkdir -p "$workDir"
+		source "${SCRIPTDIR}"/.py3env/bin/activate
+		python "${SCRIPTDIR}"/pubEnricher.py -b meta -C "${SCRIPTDIR}"/cron-config.ini -D "$workDir" --use-opeb "$toolsFileXZ"
+	fi
+	echo "${SHELL}" "${SCRIPTDIR}"/opeb-submitter/result_submitter.bash "${SCRIPTDIR}"/opeb-submitter/cron-submitter.ini "$workDir"
+else
+	echo "ERROR: This script needs two parameters: a workdir and the destination path to the input tools file" 1>&2
+fi
