809d461b4e6eed1e7c7175bb452c1f095385e854
José Mª Fernández <jose.m.fernandez@bsc.es>
2020-09-02 13:06:37 +0200

    Now input directory is recognized, and manifest file is parsed, so it only processes those contents properly indexed

--- repoEnricher/opeb-submitter/repo_result_submitter.bash
+++ repoEnricher/opeb-submitter/repo_result_submitter.bash
@@ -27,6 +27,19 @@ if [ $# -gt 1 ]; then
 	shift
 	# Loading the credentials
 	source "$config"
+	if [ -d  "$1" ] ; then
+		resdir="$1"
+		if [ -f "${resdir}/manifest.json" ] ; then
+			declare -a ifiles=( $(jq -r '.[] | .file ' "$resdir"/manifest.json ) )
+			# This is needed to resolve relative directories
+			cd "$resdir"
+		else
+			echo "Usage: $0 {config_file} {json_result_dir_with_manifest|json_files}+" 1>&2
+			exit 1
+		fi
+	else
+		declare -a ifiles=( "$@" )
+	fi
 
 	# These could have been redefined
 	OPEB_HOST="${OPEB_HOST:-openebench.bsc.es}"
@@ -38,12 +51,12 @@ if [ $# -gt 1 ]; then
 	OPEB_ALAMBIQUE_BASE="${OPEB_ALAMBIQUE_BASE:-https://${ALAMBIQUE_HOST}/monitor/alambique/}"
 
 	if [ -n "$OPEB_METRICS_USER" -a -n "$OPEB_METRICS_PASS" ]; then
-		jq --slurp --arg host "$OPEB_HOST" -f "$scriptdir"/repo_enricher2opeb.jq "$@" | \
+		jq --slurp --arg host "$OPEB_HOST" -f "$scriptdir"/repo_enricher2opeb.jq "${ifiles[@]}" | \
 		curl -v -X "$OPEB_METHOD" -u "${OPEB_METRICS_USER}":"${OPEB_METRICS_PASS}" -H 'Content-Type: application/json' \
 		"${OPEB_METRICS_BASE}" -d "@-"
 	else
 		echo "ERROR: The config file must declare the OPEB_METRICS_USER and OPEB_METRICS_PASS variables, which are the credentials to OpenEBench" 1>&2
 	fi
 else
-	echo "Usage: $0 {config_file} {json_result}+" 1>&2
+	echo "Usage: $0 {config_file} {json_result_dir_with_manifest|json_files}+" 1>&2
 fi
